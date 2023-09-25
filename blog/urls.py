e3a59c2ec2620ccb6c4feff5929eec639d50f4ed
sunicom <github@sunicom.co.jp>
2023-09-25 12:56:22 +0900

    My Django Girls app, first commit

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+#from .views import SampleAppList
+
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', views.SampleAppList.as_view()),
+    path('smaple', views.post_list, name='post_list'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Post
 
 # Create your views here.
+class SampleAppList(ListView):
+    template_name = 'blog/post_list.html'
+    model = Post
+
 
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
